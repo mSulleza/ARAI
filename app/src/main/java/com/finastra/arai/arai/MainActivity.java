ef01abf194ed19444caf1f0d1d3c712b8c33021a
mSulleza <marksulleza@gmail.com>
2018-12-06 09:36:20 +0800

    Initial commit

---
+++ app/src/main/java/com/finastra/arai/arai/MainActivity.java
@@ -0,0 +1,21 @@
+package com.finastra.arai.arai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+}
